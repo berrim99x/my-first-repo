746b49eb30138e5f9bc6b3c79e158ed1e05537c7
hakim266021 <hakimberrim29@gmail.com>
2025-10-19 20:51:17 +0100

    3

---
+++ RechSq.py
@@ -0,0 +1,10 @@
+def RechSq(T:list,taille:int,x:int) -> int:
+    for i in range(taille):
+        if T[i] == x: return i
+    return -1
+
+def main():
+    T = [1,2,3,4,5]
+    taille = len(T)
+    x = 5
+    print(RechSq(T,taille,x))
